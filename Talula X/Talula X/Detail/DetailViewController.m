380f3801fb5c47caeba367b428c92b1dcdd11e8a
Diana Brnovik <dia.brnovikova@gmail.com>
2021-05-17 17:33:52 +0200

    UI elements
    UI detail views

--- Talula X/Talula X/Detail/DetailViewController.m
+++ Talula X/Talula X/Detail/DetailViewController.m
@@ -8,6 +8,15 @@
 #import "DetailViewController.h"
 
 @interface DetailViewController ()
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *placeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *coordinatesLabel;
+@property (weak, nonatomic) IBOutlet UILabel *toCurrentPlaceLabel;
+@property (weak, nonatomic) IBOutlet UILabel *toPinnedPlaceLabel;
+@property (weak, nonatomic) IBOutlet UILabel *distanceToCurrentPlaceLabel;
+@property (weak, nonatomic) IBOutlet UILabel *distanceToPinnedPlaceLabel;
+@property (weak, nonatomic) IBOutlet UILabel *yearLabel;
 
 @end
 
